e65a96d1dc9b356a669bb2616e33b7ed1cefc539
Ian Easton <ijeaston@gmail.com>
2021-12-05 09:51:29 -0500

    Add day 5

---
+++ day5.py
@@ -0,0 +1,44 @@
+def problem_1(points):
+    lines = [extrapolate_line(*p) for p in points if is_straight(*p)]
+    return len(find_overlaps(lines))
+
+
+def problem_2(points):
+    lines = [extrapolate_line(*p) for p in points]
+    return len(find_overlaps(lines))
+
+
+def find_overlaps(lines):
+    overlap = set()
+    seen = set()
+
+    for x, line_a in enumerate(lines):
+        for y, lines_b in enumerate(lines):
+            pair = tuple(sorted((x,y)))
+            if x != y and pair not in seen:
+                overlap = overlap.union(set.intersection(line_a, lines_b))
+                seen.add(pair)
+    return overlap
+
+
+def extrapolate_line(start, end):
+    if not is_straight(start, end):
+        return set(zip(inclusive_range(start[0], end[0]), inclusive_range(start[1], end[1])))
+    elif is_vertical(start, end):
+        return set([(start[0], y) for y in inclusive_range(start[1], end[1])])
+    else:
+        return set([(x, start[1]) for x in inclusive_range(start[0], end[0])])
+
+
+def is_vertical(start, end):
+    return start[0] == end[0]
+
+def is_straight(start, end):
+    return start[0] == end[0] or start[1] == end[1]
+
+
+def inclusive_range(x, y):
+    if x < y:
+        return range(x, y + 1)
+    else:
+        return range(x, y - 1, -1)
